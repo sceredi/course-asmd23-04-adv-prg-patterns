27ae2fe80f2ccb0fd853fb07ca323469099d4bca
sceredi <ceredi.simone.iti@gmail.com>
2024-03-18 10:00:41 +0100

    chore: implement nix stuff

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "A flake for Scala development";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell =
+          pkgs.mkShell { buildInputs = with pkgs; [ sbt metals jdk17 ]; };
+      });
+}
