4da0802b24539f43d2a6a78781bc91b0988a40c1
sceredi <ceredi.simone.iti@gmail.com>
2024-03-19 12:40:33 +0100

    feat: added some more property tests

--- src/test/scala/u04/adts/SequenceADTCheck.scala
+++ src/test/scala/u04/adts/SequenceADTCheck.scala
@@ -19,3 +19,21 @@ object SequenceADTCheck extends Properties("sequenceADT"):
   property("map identity") = forAll(sequenceGen[Int]) { seq =>
     seq.map(el => el) == seq
   }
+
+  property("map composition") =
+    forAll(sequenceGen[Int], ((el: Int) => el + 1), ((el: Int) => el * 2)) {
+      (seq, f, g) =>
+        seq.map(f).map(g) == seq.map(f andThen g)
+    }
+
+  property("map flatmap similarity") = forAll(sequenceGen[Int]) { seq =>
+    seq.map(_ + 1) == seq.flatMap(el => cons(el + 1, nil()))
+  }
+
+  property("flatMap associativity") = forAll(
+    sequenceGen[Int],
+    ((el: Int) => cons(el + 1, nil())),
+    ((el: Int) => cons(el * 2, nil()))
+  ) { (seq, f, g) =>
+    seq.flatMap(f).flatMap(g) == seq.flatMap(el => f(el).flatMap(g))
+  }
